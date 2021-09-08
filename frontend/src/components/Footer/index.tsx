6e44dbc68db000e8dcb453437a090222702c4984
Ze Luiz <joseluizmartinsf@gmail.com>
2021-09-08 12:10:55 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/zeluiz78" target="_blank" rel="noreferrer">José Luiz Martins</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://www.linkedin.com/in/jose-luiz-martins-62129014a/" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
